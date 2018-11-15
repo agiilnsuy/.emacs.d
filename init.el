280a1d57098e0de7072d789138c6812a5be2c59e
lsyrup <38693868+lsyrup@users.noreply.github.com>
2018-11-15 15:13:39 +0800

    Add files via upload

--- init.el
+++ init.el
@@ -1,34 +1,79 @@
-;;; init.el --- Siyang Liu
+;;------------------------------------------------------------
+;; Package Initialize
+;;------------------------------------------------------------
 
-;;; Commentary:
-;;  A minimal init.el for emacs.
-
-;;; Code:
-
-;; package initialize
-(require 'package)
+(package-initialize)
 (setq package-archives
       '(("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
-(setq package-enable-at-startup nil)
-(package-initialize)
 
-;; basics
-(setq inhibit-startup-message t)        ; no welcome window
-(setq make-backup-files nil)            ; no backup~ files
-(setq auto-save-default nil)            ; no #auto-save# files
-(setq ring-bell-function 'ignore)       ; turn off bell sound
-(setq-default indent-tabs-mode nil)     ; use spaces instead of tabs
-(electric-pair-mode 1)                  ; auto-pair parentheses
-(show-paren-mode 1)                     ; hightlight parentheses
+;;------------------------------------------------------------
+;; Basics
+;;------------------------------------------------------------
+
+;; Hide welcome message
+(setq inhibit-startup-message t)
+
+;; Prevent from generating back-up~ file
+(setq make-backup-files nil)
+
+;; Prevent from generating #auto-save# file
+(setq auto-save-default nil)
+
+;;------------------------------------------------------------
+;; Syntax
+;;------------------------------------------------------------
+
+;; Use spaces to indent
+(setq-default indent-tabs-mode nil)
+
+;; Delete trailing spaces when saving
 (add-hook 'before-save-hook
-          'delete-trailing-whitespace)  ; delete trailing spaces on saving
-(setq default-frame-alist
-      '((font . "Menlo 14")))           ; set default font
+          'delete-trailing-whitespace)
 
-;; enable ido mode
+;;------------------------------------------------------------
+;; Visual
+;;------------------------------------------------------------
+
+;; Highlight paired parentheses
+(show-paren-mode 1)
+
+;; Show relative line numbers
+(global-display-line-numbers-mode)
+(setq display-line-numbers-type 'relative)
+
+;;------------------------------------------------------------
+;; Build-in Modes
+;;------------------------------------------------------------
+
+;; Enable ido mode
 (ido-mode 1)
-(ido-everywhere)
 (setq ido-enable-flex-matching t)
 
-;;; init.el ends here
+;; Enable icomplete mode
+(icomplete-mode 1)
+
+;;------------------------------------------------------------
+;; References
+;;------------------------------------------------------------
+
+;; https://github.com/emacs-tw/awesome-emacs
+
+;;------------------------------------------------------------
+;; Pakcage Custom Settings
+;;------------------------------------------------------------
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (lua-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; init.el ends here.
